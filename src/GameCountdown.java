27305941670bc2332fb16f4c9d9d9aadab7037f2
toniname <drednoyta1@ukr.net>
2025-07-21 15:41:24 +0300

    init commit

---
+++ src/GameCountdown.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class GameCountdown {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // === 1. Створюємо планувальник на 1 потік ===
+
+
+            // === 2. Кількість секунд для відліку ===
+
+
+            // === 3. Задача, яка виконується щосекунди ===
+
+
+            // === 4. Запускаємо задачу кожну секунду без початкової затримки ===
+
+
+            // === 5. Очікуємо завершення відліку, щоб не завершити програму раніше часу ===
+
+
+
+    }
+
+}
--- src/HelloThread.java
+++ src/HelloThread.java
@@ -4,5 +4,4 @@ public class HelloThread extends Thread{
     public void run(){
         System.out.println("Hello from thread_2!");
     }
-
 }
